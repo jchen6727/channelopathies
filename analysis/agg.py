10cdcd6e8a58ddb465ca27ebd0d4b622f2d22765
jchen_lethe <jchen.6727@gmail.com>
2023-12-22 12:56:06 -0500

    changes

---
+++ analysis/agg.py
@@ -0,0 +1,7 @@
+import utils
+
+
+dataFolder = '../data/'
+batchLabel = 'batch_2023-12-13'   # v52_batch3'
+
+params, data = utils.readBatchData(dataFolder, batchLabel, loadAll=False, saveAll=True, vars=[('simData','V_soma')], maxCombs=None) 
